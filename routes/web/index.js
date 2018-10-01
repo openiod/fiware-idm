cf1c5151f281efc0bd456a385c1f06739191c4f8
awiel <awiel@scapeler.com>
2018-10-01 09:35:16 +0200

    restore web/index

--- routes/web/index.js
+++ routes/web/index.js
@@ -4,8 +4,6 @@ var bodyParser = require('body-parser');
 
 var router = express.Router();
 
-var config = require ('./config.js');
-
 var csrfProtection = csrf({ cookie: true })
 
 // Create controllers
@@ -22,9 +20,9 @@ router.use( function( req, res, next ) {
 });
 
 // Get Home Page
-router.get(config.urlPrefix+'/', csrfProtection, function(req, res, next) {
+router.get('/', csrfProtection, function(req, res, next) {
 	if (req.session.user) {
-        res.redirect(config.urlPrefix+'/idm')
+        res.redirect('/idm')
   } else {
   	res.render('index', { errors: [], csrfToken: req.csrfToken() });
   }
@@ -32,21 +30,21 @@ router.get(config.urlPrefix+'/', csrfProtection, function(req, res, next) {
 
 // Routes when user is logged
 //  - Create sessions for users
-router.use(config.urlPrefix+'/auth',  require('./authenticate'))
+router.use('/auth',  require('./authenticate'))
 
 //  - Route to warn user to change password
-router.get(config.urlPrefix+'/update_password',      web_session_controller.login_required,   csrfProtection,    web_session_controller.update_password)
+router.get('/update_password',      web_session_controller.login_required,   csrfProtection,    web_session_controller.update_password)
 
 //  - Routes when user is logged
-router.use(config.urlPrefix+'/idm/admins',           web_session_controller.login_required, web_session_controller.password_check_date, web_admin_controller.is_admin, require('./admins'))
-router.use(config.urlPrefix+'/idm/applications',     web_session_controller.login_required, web_session_controller.password_check_date, require('./applications'))
-router.use(config.urlPrefix+'/idm/users',            web_session_controller.login_required, web_session_controller.password_check_date, require('./users'))
-router.use(config.urlPrefix+'/idm/organizations',    web_session_controller.login_required, web_session_controller.password_check_date, require('./organizations'))
-router.use(config.urlPrefix+'/idm/settings',         web_session_controller.login_required, require('./settings'))
-router.use(config.urlPrefix+'/idm',                  web_session_controller.login_required, web_session_controller.password_check_date, csrfProtection,  require('./homes'))
+router.use('/idm/admins',           web_session_controller.login_required, web_session_controller.password_check_date, web_admin_controller.is_admin, require('./admins'))
+router.use('/idm/applications',     web_session_controller.login_required, web_session_controller.password_check_date, require('./applications'))
+router.use('/idm/users',            web_session_controller.login_required, web_session_controller.password_check_date, require('./users'))
+router.use('/idm/organizations',    web_session_controller.login_required, web_session_controller.password_check_date, require('./organizations'))
+router.use('/idm/settings',         web_session_controller.login_required, require('./settings'))
+router.use('/idm',                  web_session_controller.login_required, web_session_controller.password_check_date, csrfProtection,  require('./homes'))
 
 // -- Routes when user is not logged
-router.use(config.urlPrefix+'/',  web_session_controller.login_not_required, require('./not_authenticate'))
+router.use('/',  web_session_controller.login_not_required, require('./not_authenticate'))
 
 // catch 404 and forward to error handler
 router.use(function(req, res, next) {
